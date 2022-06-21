3e09639d82c3466f3231b6232a270be057ef1ff8
saidugurlu <ugurlumsaid@gmx.de>
2022-06-21 10:12:48 +0200

    api finished

--- server.js
+++ server.js
@@ -3,13 +3,47 @@ import { siteData } from './src/models.js';
 
 const app = express();
 const port = process.env.PORT || 3007;
+const fullUrl = `http://localhost:${port}`;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send(siteData);
-});
-
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
-});
+	res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+     <title>Info API</title>
+    <style>
+     body {
+         background-color: #ccc;
+         font-family: monospace;
+         font-size: 1.5rem;
+     }
+     a {
+         color: #777;
+     }
+    </style>
+    </head>
+    <body>
+    <h1>Info API</h1> 
+    <ul>
+    ${Object.keys(siteData).map((key) => {
+         return `<li><a href="${fullUrl}/${key}">${fullUrl}/${key}</a></li>`;
+    }).join('')}
+    </ul>
+    </body>
+    </html> 
+     `);
+ });
+ 
+ Object.entries(siteData).forEach(entry => {
+     const key = entry[0];
+     const value = entry[1];
+     app.get(`/${key}`, (req, res) => {
+         res.send(value);
+     });
+ })
+ 
+ app.listen(port, () => {
+     console.log(`Listening at http://localhost:${port}`);
+ });
