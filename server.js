6e7a9052b010b8994b98f9b5424673b375929c14
saidugurlu <ugurlumsaid@gmx.de>
2022-06-21 09:58:04 +0200

    sending all data with base root /

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { siteData } from './src/models.js';
-import { siteView } from './src/views.js';
 
 const app = express();
 const port = process.env.PORT || 3007;
@@ -8,7 +7,7 @@ const port = process.env.PORT || 3007;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send(siteView(siteData));
+    res.send(siteData);
 });
 
 app.listen(port, () => {
